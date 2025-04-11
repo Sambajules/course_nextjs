a57bf1d20ed73c4f18e9332fc073d2da4f28900f
Sambajules <cesarjules237@gmail.com>
2025-04-11 22:48:37 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Lusitana, Manrope } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+
+/*export const manrope = Manrope({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+*/
